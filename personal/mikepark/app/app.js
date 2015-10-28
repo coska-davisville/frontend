2f8e73b11a33137957fd049d6f461f487cb24514
Mike Sehui Park <mike.sayhi@gmail.com>
2015-10-28 13:37:54 -0400

    added angular-ui-router

--- personal/mikepark/app/app.js
+++ personal/mikepark/app/app.js
@@ -1,12 +1,35 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
+var myApp = angular.module('myApp', [
+  'ui.router',
+  
   'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+])
+
+.config(function($stateProvider, $urlRouterProvider) {
+  
+  //
+  // For any unmatched url, redirect to /state1
+  $urlRouterProvider.otherwise("/home");
+  //
+  // Now set up the states
+  $stateProvider
+    .state('home', {
+      url: "/home",
+      templateUrl: "tpls/home.tpl.html",
+      controller: 'HomeController'
+    })
+    .state('profile', {
+      url: "/profile",
+      templateUrl: "tpls/profile.tpl.html"
+    })
+    .state('profile.list', {
+      url: "/profile.list",
+      templateUrl: "tpls/profile.list.tpl.html",
+      controller: function($scope) {
+        $scope.items = ["A", "List", "Of", "Items"];
+      }
+    });
+    
+});
