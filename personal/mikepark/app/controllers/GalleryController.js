080fa609b313c1b0ad42ada414a3a38a88c518b3
Mike Sehui Park <mike.sayhi@gmail.com>
2015-11-05 18:24:51 -0500

    Router example

--- personal/mikepark/app/controllers/GalleryController.js
+++ personal/mikepark/app/controllers/GalleryController.js
@@ -1,8 +1,13 @@
 'use strict';
 
 /* Case study 1 */
-myApp
-
-.controller('GalleryController', function(){
+myApp.controller('GalleryController', ['$scope', '$state', function($scope, $state){
 	
-});
+    
+    
+    $scope.goHome = function(){
+        
+        $state.go('home') ;       
+    };
+    
+}]);
